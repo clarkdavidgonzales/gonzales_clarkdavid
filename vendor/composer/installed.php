1a3dff5ca6b56e4d79aec9c81652f895574fdc88
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-15 16:08:25 +0800

    Added composer.json and DockerFile

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '42f52c8b0bd3261458b31c3be9a20c3064ad39a9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '42f52c8b0bd3261458b31c3be9a20c3064ad39a9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
