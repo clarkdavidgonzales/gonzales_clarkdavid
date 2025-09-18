a6891a4863fd42adf2b1fd53709b3e50dfffc0ed
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-18 09:48:43 +0800

    updated database

--- app/config/database.php
+++ app/config/database.php
@@ -59,10 +59,10 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> 'clark13',
+    'username'	=> 'sql12799073',
+    'password'	=> 'vgtKU9h8iE',
     'database'	=> 'mockdata',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
