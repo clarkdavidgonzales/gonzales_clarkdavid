c970b23cf5f3e33787a96af4e19c147f567afbc7
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-28 12:57:33 +0800

    Fixing database

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799073',
-    'password'	=> 'vgtKU9h8iE',
-    'database'	=> 'sql12799073',
+    'username'	=> 'sql12800363',
+    'password'	=> 'Iu2xqY88CA',
+    'database'	=> 'sql12800363',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
