66a884959680cd5d47194b5e4a5608af73eb61bb
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-28 16:47:53 +0800

    fixing routes to original

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,31 +5,49 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
  * LavaLust - an opensource lightweight PHP MVC Framework
  * ------------------------------------------------------------------
  *
- * (Original LavaLust Copyright Notice)
+ * MIT License
+ *
+ * Copyright (c) 2020 Ronald M. Marasigan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package LavaLust
+ * @author Ronald M. Marasigan <ronald.marasigan@yahoo.com>
+ * @since Version 1
+ * @link https://github.com/ronmarasigan/LavaLust
+ * @license https://opensource.org/licenses/MIT MIT License
  */
 
 /*
 | -------------------------------------------------------------------
 | URI ROUTING
 | -------------------------------------------------------------------
+| Here is where you can register web routes for your application.
+|
+|
 */
-
-// Home route: handles / or /1, /2, etc. (Pagination)
-$router->get('/{page?}', 'UserController::show'); 
-
+$router->get('/', 'UserController::show');
 $router->get('/about', 'Welcome::about');
 $router->get('/user/profile/{username}/{name}', 'UserController::profile');
-
-// Explicit show route: handles /user/show or /user/show/1, /user/show/2, etc. (Pagination)
-$router->get('/user/show/{page?}', 'UserController::show'); 
-
+$router->get('/user/show', 'UserController::show');
 $router->match('/user/create', 'UserController::create', ['GET', 'POST']);
 $router->match('/user/update/{id}', 'UserController::update', ['GET', 'POST']);
-
-// ✅ FIX: Delete route must accept the optional page number for redirects
-$router->get('/user/delete/{id}/{page?}', 'UserController::delete');
-
-// ✅ FIX: Soft-delete route must accept the optional page number for redirects
-$router->get('user/soft-delete/{id}/{page?}', 'UserController::soft_delete'); 
-
+$router->get('/user/delete/{id}', 'UserController::delete');
+$router->get('user/soft-delete/{id}', 'UserController::soft_delete');
 $router->get('/user/restore/{id}', 'UserController::restore');
