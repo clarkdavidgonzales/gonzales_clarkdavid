040a17b513914434c7f45466a9fb80c9f2af23dc
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-28 17:23:58 +0800

    fixing again

--- app/config/database.php
+++ app/config/database.php
@@ -64,7 +64,7 @@ $database['main'] = array(
     'username'	=> 'sql12800363',
     'password'	=> 'Iu2xqY88CA',
     'database'	=> 'sql12800363',
-    'charset'	=> 'utf8mb4',ggg
+    'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
     'path'      => ''
