42237bc25b48a42569e142755c3e90a8f5d22ae4
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-28 15:27:37 +0800

    fixed to original

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'sql12.freesqldatabase.com',
-    'port'		=> '3306',
-    'username'	=> 'sql12800363',
-    'password'	=> 'Iu2xqY88CA',
-    'database'	=> 'sql12800363',
+    'hostname'	=> 'localhost',
+    'port'		=> '',
+    'username'	=> 'root',
+    'password'	=> '',
+    'database'	=> 'mockdata',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,10 +1,18 @@
-<?php namespace App\Models;
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-use CodeIgniter\Model;
+/**
+ * Model: UserModel
+ * 
+ * Automatically generated via CLI.
+ */
+class UserModel extends Model {
+    protected $table = 'students';
+    protected $primary_key = 'id';
 
-class StudentModel extends Model
-{
-    protected $table      = 'students'; // **Change to your actual table name**
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['last_name', 'first_name', 'email'];
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
 }
