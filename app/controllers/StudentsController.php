57cb46d74b617c33df80f7b22d399f62082ae054
clarkdavidgonzales <clarkgonzales93@gmail.com>
2025-09-29 18:14:58 +0800

    fixing controllers

--- app/controllers/StudentsController.php
+++ app/controllers/StudentsController.php
@@ -2,11 +2,11 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-class UsersController extends Controller {
+class StudentsController extends Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->call->model('UsersModel');
+        $this->call->model('StudentsModel');
     }
 
     public function index()
